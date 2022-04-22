2baa5a53589a6f64eb4070d5b854489bd7446815
AntonioGSC <antoniogabrielagsc@gmail.com>
2022-04-22 02:00:51 -0300

    Get by ID Alunos

--- controllers/controller.go
+++ controllers/controller.go
@@ -8,15 +8,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, models.Alunos)
-}
-
 func Saudacao(c *gin.Context) {
 	nome := c.Params.ByName("nome")
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
+}
+
+func ExibeTodosAlunos(c *gin.Context) {
+	var alunos []models.Aluno
+	database.DB.Find(&alunos)
+	c.JSON(http.StatusOK, alunos)
+}
+
+func RetornaUmAluno(c *gin.Context) {
+	var aluno models.Aluno
+	id := c.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not Found": "Aluno não encontrado",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, aluno)
 }
 
 func CriaNovoAluno(c *gin.Context) {
--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"github.com/AntonioGSC/api-go-gin/database"
-	"github.com/AntonioGSC/api-go-gin/models"
 	"github.com/AntonioGSC/api-go-gin/routes"
 )
 
 func main() {
 	database.ConectaComDB()
-	models.Alunos = []models.Aluno{
-		{Nome: "Tony", CPF: "00000000000", RG: "000000000"},
-		{Nome: "Ana", CPF: "11111111111", RG: "100000000"},
-	}
 	routes.HandleRequests()
 }
